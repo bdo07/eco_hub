49cded22459be1723ec358501e34a16789e51d89
abderrahman <arid07abderrahman@gmail.com>
2026-05-27 00:54:32 +0100

    fix: update mutator path for custom fetch in orval configuration

--- backend/packages/api-spec/orval.config.ts
+++ backend/packages/api-spec/orval.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
           includeHttpResponseReturnType: false,
         },
         mutator: {
-          path: path.resolve(apiClientReactSrc, "custom-fetch.ts"),
+          path: "../../../../frontend/packages/api-client-react/src/custom-fetch.ts",
           name: "customFetch",
         },
       },
